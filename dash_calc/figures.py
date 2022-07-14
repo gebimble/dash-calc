de83cf7f2505be8216e84c0cf75b0866468c0b57
Joseph Beaver <joseph.beaver@gmail.com>
2022-07-14 11:23:42 +0100

    Fixed the empty `px.` figures - now `go.Figure` objects instead!

--- dash_calc/figures.py
+++ dash_calc/figures.py
@@ -3,7 +3,7 @@ import plotly.graph_objects as go
 
 main_fig = go.Figure()
 
-line_fig = px.scatter()
+line_fig = go.Figure()
 line_fig.update_xaxes(autorange='reversed')
 
-hist_fig = px.histogram()
+hist_fig = go.Figure()
